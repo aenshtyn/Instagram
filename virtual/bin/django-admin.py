8ca4c0dd2dc211b3ca3756e30dd51d7ec39b5994
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-05-30 20:55:12 +0300

    initial project setup

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/mohamed/Documents/Moringa-School-Projects/Moringa Core/Week 9/IP/Instagram/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
