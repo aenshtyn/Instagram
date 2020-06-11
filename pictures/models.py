b53298fee4c9320dfb75a60a3f3e94b74523e0b5
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-06-11 16:51:52 +0300

    crated two models for the image and profile

--- pictures/models.py
+++ pictures/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    email = models.EmailField()
+    password = models.PasswordField()
+    picture = models.ImageField()
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.first_name
+
+class Image(models.Model):
+    image = models.ImageField()
+    name = models.CharField()
+    caption = models.CharField()
+    profile = models.Foreignkey(Profile)
+    likes = models.
+    comments = models.
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.image
+    class Meta:
+        ordering = ['pub_date']
