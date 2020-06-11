c95307fe40b17998261fa4e085e6fdd9c29b2ad6
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-06-11 18:11:45 +0300

    py files

--- instagram/urls.py
+++ instagram/urls.py
@@ -20,4 +20,5 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'',include('pictures.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
